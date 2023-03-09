4c49de4e6f973f9f3751cfdbad258d5a6dce7118
jcastellanog <jesus.castellano@hotmail.com>
2023-03-09 09:25:29 +0100

    Agregar archivos de proyecto.

---
+++ Ejemplo02_JesusCG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejemplo02_JesusCG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
