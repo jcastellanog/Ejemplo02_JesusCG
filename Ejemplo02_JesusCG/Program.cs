c2f4a9ae88e6f3260369f280f1d45b792229e21b
jcastellanog <jesus.castellano@hotmail.com>
2023-03-09 09:43:09 +0100

    Estructura del Programa

--- Ejemplo02_JesusCG/Program.cs
+++ Ejemplo02_JesusCG/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // CONSTANTES
+
+            // VARIABLES
+
+            // INICIALIZACIÓN
+
+            // ENTRADA
+            
+            // PROCESO
+
+            // SALIDA
+
         }
     }
 }
